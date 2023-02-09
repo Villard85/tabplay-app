c2dabf9b50a7d64e09ffe67370b0f84ffb36e7bd
Valera <valprud@rambler.ru>
2023-02-09 14:31:17 +0300

    Now use session to store app data instead of global variables. POST-Redrect-GET scheme added to "pack" method to prevent browser prompt on page refresh

--- tabplay/views/views.py
+++ tabplay/views/views.py
@@ -64,102 +64,116 @@ def greedy(item_list, v_limit):
 				break
 	return final_list
 
-items = {}
-
 @app.route('/', methods=['GET'])
 def index():
-	global items
-	return render_template('index.html', items=items)
+	if session.get('items') == None:
+		session['items'] = {}
+	return render_template('index.html', items=session.get('items'))
 
 @app.route('/', methods=['POST'])
 def add_item():
-	global items
+	#global items
+	items = session.get('items')
 	fields = request.form
 	new_id = randint(10000, 90000)
 	while new_id in items.keys():
 		new_id = randint(10000, 90000)
-	items[new_id] =	{'id': new_id, 'item': fields['item-name'], 
+	items[str(new_id)] = {'id': new_id, 'item': fields['item-name'], 
 	'vol': float(fields['item-vol']), 'val': float(fields['item-val'])} 
 	flash('Предмет \"{}\" добавлен'.format(fields['item-name']), 'good')
+	session['items'] = items
 	return redirect(url_for('index'))
 
-@app.route('/packed', methods=['POST'])
+@app.route('/packed', methods=['GET','POST'])
 def pack():
-	global items
-	if len(items)==0:
-		flash("В списке нет ни одного предмета", "bad")
-		return redirect(url_for('index'))
-	fields = request.form
-	v_limit = float(fields['total-vol'])
-	item_list = [*items.values()]
-	if min_vol(item_list)>v_limit:
-		solution=[]
+	if request.method=='POST':
+		items = session.get('items')
+		if len(items)==0:
+			flash("В списке нет ни одного предмета", "bad")
+			return redirect(url_for('index'))
+		fields = request.form
+		v_limit = float(fields['total-vol'])
+		item_list = [*items.values()]
+		if min_vol(item_list)>v_limit:
+			solution=[]
+		else:
+			population, generations = genetic.run_evolution(
+				populate_func=partial(genetic.generate_population, size=4, genome_length=len(item_list)),
+				fitness_func=partial(fitness, item_list=item_list, v_limit=v_limit),
+				fitness_limit=tot_val(item_list),
+				generation_limit=100
+				#printer=genetic.print_stats
+			)
+			solution = items_from_genome(population[0], item_list)
+		tv = tot_vol(solution)
+		g_solution = greedy(item_list, v_limit)
+		if tot_val(g_solution)>tot_val(solution) or tot_vol(solution)>v_limit:
+			solution = g_solution
+			flash(">--Жадное решение--<", "blue")
+		else:
+			flash('Рюкзак упакован', 'good')
+		res={}
+		res['limit']=v_limit
+		res['names']=name_list(solution)
+		res['vol']=tot_vol(solution)
+		res['val']=tot_val(solution)
+		session['res']=res
+		return redirect(url_for('pack'))
 	else:
-		population, generations = genetic.run_evolution(
-			populate_func=partial(genetic.generate_population, size=4, genome_length=len(item_list)),
-			fitness_func=partial(fitness, item_list=item_list, v_limit=v_limit),
-			fitness_limit=tot_val(item_list),
-			generation_limit=100
-			#printer=genetic.print_stats
-		)
-		solution = items_from_genome(population[0], item_list)
-	tv = tot_vol(solution)
-	g_solution = greedy(item_list, v_limit)
-	if tot_val(g_solution)>tot_val(solution) or tot_vol(solution)>v_limit:
-		solution = g_solution
-		flash(">--Жадное решение--<", "blue")
-	else:
-		flash('Рюкзак упакован', 'good')
-	res={}
-	res['limit']=v_limit
-	res['names']=name_list(solution)
-	res['vol']=tot_vol(solution)
-	res['val']=tot_val(solution)
-	session['res']=res
-	return render_template("results.html", res=res)
+		return render_template("results.html", res=session.get('res'))
+	
+
+	return render_template("results.html", res=session.get('res'))
 
 @app.route('/api/data')
 def data():
+	items = session.get('items')
 	return {'data': [*items.values()]}
 	
 @app.route('/api/data', methods=['POST'])
 def update():
-	global items
+	items = session.get('items')
 	data = request.get_json()
 	print(data)
 	if 'id' not in data:
 		abort(400)
-	item = items[int(data['id'])]
+	item = items[data['id']]
 	for field in ['item', 'vol', 'val']:
 		if field in data:
 			if field=='vol' or field=='val':
 				try:
-					item[field]=round(float(data[field]),1)
+					item[field] = round(float(data[field]),1)
 				except Exception:
 					pass
 			else:
-				item[field]=data[field]
+				item[field] = data[field]
+	session['items'] = items
+	print(items)
 	return '', 204
 	
 @app.route('/api/delete', methods=['POST'])
 def delete():
-	global items
+	#global items
+	items = session.get('items')
 	data = request.get_json()
 	if 'id' not in data:
 		abort(400)
-	items.pop(int(data['id']), None)
+	del items[str(data['id'])]
+	session['items'] = items
 	return '', 204
 	
 @app.route('/clear')
 def clear():
-	global items
+	#global items
 	items={}
+	session['items']={}
 	flash("Список очищен", "good")
 	return redirect(url_for('index'))
 	
 @app.route('/load', methods=['POST'])
 def read_file():
-	global items
+	#global items
+	items = session.get('items')
 	i_list = request.files['item-file']
 	f_name = i_list.filename
 	if f_name != '':
@@ -187,6 +201,7 @@ def read_file():
 			except Exception:
 				pass
 		flash("Файл прочитан, добавлено {} предметов".format(count), "good")
+		session['items'] = items
 		return redirect(url_for('index'))
 		# except Exception:
 			# flash("Неверный формат файла", "bad")
